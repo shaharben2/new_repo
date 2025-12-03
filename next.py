3db973d875c14cd4bfb03bf6137193590819c75f
shaharben2 <shtsava@gmail.com>
2025-12-03 15:46:49 +0200

    part 5

--- next.py
+++ next.py
@@ -2,9 +2,9 @@ import functools
 
 
 def main():
-    with open(input("enter path: "), "r") as file1:
-        with open(input("enter path: "), "w") as file2:
-            file2.write("\n".join(map(str, map(len, file1.read().split("\n")))))
+    with open(input("enter path: "), "r") as file:
+        search_len = int(input("Enter length: "))
+        print("\n".join(list(filter(lambda name: len(name) == search_len, file.read().split("\n")))))
         
 
 if __name__ == '__main__':
