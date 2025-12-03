3ffb1a229f6c38d91d7778df9d360a2c409b06bf
shaharben2 <shtsava@gmail.com>
2025-12-03 15:42:55 +0200

    part 4

--- next.py
+++ next.py
@@ -2,10 +2,9 @@ import functools
 
 
 def main():
-    with open(input("enter path: "), "r") as file:
-        read_file = file.read().split("\n")
-        min_len = len(min(read_file, key = len))
-        print("\n".join(list(filter(lambda name: len(name) == min_len, read_file))))
+    with open(input("enter path: "), "r") as file1:
+        with open(input("enter path: "), "w") as file2:
+            file2.write("\n".join(map(str, map(len, file1.read().split("\n")))))
         
 
 if __name__ == '__main__':
